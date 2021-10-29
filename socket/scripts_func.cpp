5a9f7aec6c60b845280c40d809c5a71b15dc4176
hsy <1016743781@qq.com>
2021-10-29 12:46:13 +0800

    master

--- socket/scripts_func.cpp
+++ socket/scripts_func.cpp
@@ -27,7 +27,7 @@ bool Writen(const int sockfd, const char *buffer, const size_t n){
     return true;
 }
 
-bool TcpRead(const int sockfd, char *buffer, int *ibuflen, const int itimeout = 0){
+bool TcpRead(const int sockfd, char *buffer, int *ibuflen, const int itimeout){
     if (sockfd == -1) return false;
 
     if (itimeout > 0){
@@ -40,7 +40,7 @@ bool TcpRead(const int sockfd, char *buffer, int *ibuflen, const int itimeout = 
         timeout.tv_sec = itimeout; timeout.tv_usec = 0;
 
         int i;
-        if ( (i = select(sockfd+1,&tmpfd,0,0,&timeout)) <= 0 ) return false;
+        if ( (i = select(sockfd+1, &tmpfd, 0, 0, &timeout)) <= 0 ) return false;
     }
 
     (*ibuflen) = 0;
@@ -53,6 +53,38 @@ bool TcpRead(const int sockfd, char *buffer, int *ibuflen, const int itimeout = 
     return true;
 }
 
+bool TcpWrite(const int sockfd, const char *buffer,const int ibuflen){
+    if(sockfd == -1) return false;
+
+    fd_set tmpfd;
+
+    FD_ZERO(&tmpfd);
+    FD_SET(sockfd, &tmpfd);
+
+    struct timeval timeout;
+    timeout.tv_sec = 5; timeout.tv_usec = 0;
+
+    if(select(sockfd + 1, 0, &tmpfd, 0, &timeout) <= 0) return false;
+
+    int _len = 0;
+
+    if(ibuflen == 0){
+        _len = strlen(buffer);
+    }else{
+        _len = ibuflen;
+    }
+
+    int _len_n = htonl(_len);
+
+    char str_buffer[_len + 4];
+    memset(str_buffer, 0, sizeof(str_buffer));
+    memcpy(str_buffer, &_len_n, 4);
+    memcpy(str_buffer + 4, &buffer, _len);
+
+    if(Writen(sockfd, str_buffer, _len + 4) == false) return false;
+
+    return true;
+}
 
 int main(int argc, char **argv){
     return 0;
