c945e7673e9f3d2358af14588bb49c5f2fd6f697
hsy <1016743781@qq.com>
2021-11-01 22:41:17 +0800

    master

--- socket/scripts_func.cpp
+++ socket/scripts_func.cpp
@@ -86,7 +86,116 @@ bool TcpWrite(const int sockfd, const char *buffer,const int ibuflen){
     return true;
 }
 
+TcpServer::TcpServer(){
+    m_listenfd = -1;
+    m_connfd = -1;
+    m_socklen = 0;
+    m_btimeout = false;
+}
+
+bool TcpServer::initServer(const unsigned int port){
+    if(m_listenfd > 0){
+        close(m_listenfd);
+        m_listenfd = -1;
+    }
+
+    m_listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&m_servaddr, 0, sizeof(m_servaddr));
+    m_servaddr.sin_family = AF_INET;
+    m_servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    m_servaddr.sin_port = htons(port);
+
+    if(bind(m_listenfd, (struct sockaddr *)&m_servaddr, sizeof(m_servaddr)) != 0){
+        closeListen();
+        return false;
+    }
+
+    if(listen(m_listenfd, 5) != 0){
+        closeListen();
+        return false;
+    }
+
+    m_socklen = sizeof(struct sockaddr_in);
+    return true;
+}
+
+bool TcpServer::tcpAccept(){
+    if(m_listenfd == -1) return false;
+
+    if(m_connfd = accept(m_listenfd, (struct sockaddr *)&m_clientaddr, (socklen_t *)&m_socklen) < 0){
+        return false;
+    }
+    return true;
+}
+
+char *TcpServer::getIp(){
+    return(inet_ntoa(m_clientaddr.sin_addr));
+}
+
+bool TcpServer::tcpRead(char *buffer, const int itimeout){
+    if(m_connfd == -1) return false;
+
+    fd_set tmpfd;
+
+    FD_ZERO(&tmpfd);
+    FD_SET(m_connfd, &tmpfd);
+
+    struct timeval timeout;
+    timeout.tv_sec = itimeout, timeout.tv_usec = 0;
+
+    m_btimeout = false;
+    int tmpi;
+    if((tmpi = select(m_connfd + 1, &tmpfd, 0, 0, &timeout)) <= 0){
+        if(tmpi == 0) m_btimeout = true;
+        return false;
+    }
+
+    m_bufflen = 0;
+
+    return TcpRead(m_connfd, buffer, &m_bufflen);
+}
+
+bool TcpServer::tcpWrite(const char *buffer, const int ibufflen){
+    if(m_connfd == -1) return false;
+
+    fd_set tmpfd;
+
+    FD_ZERO(&tmpfd);
+    FD_SET(m_connfd, &tmpfd);
+
+    struct timeval timeout;
+    timeout.tv_sec = 5, timeout.tv_usec = 0;
+
+    m_btimeout = false;
+    int tmpi;
+    if((tmpi = select(m_connfd + 1, &tmpfd, 0, 0, &timeout)) <= 0){
+        if(tmpi == 0) m_btimeout = true;
+        return false;
+    }
+
+    int len = ibufflen == 0 ? strlen(buffer) : ibufflen;
+
+    return TcpWrite(m_connfd, buffer, len);
+}
+
+void TcpServer::closeListen(){
+    if(m_listenfd > 0){
+        close(m_listenfd);
+        m_listenfd = -1;
+    }
+}
+
+void TcpServer::closeClient(){
+    if(m_connfd > 0){
+        close(m_connfd);
+        m_connfd = -1;
+    }
+}
+
 int main(int argc, char **argv){
+
+
     return 0;
 }
 
--- socket/scripts_func.h
+++ socket/scripts_func.h
@@ -1,8 +1,13 @@
+#pragma once
+
+#include <unistd.h>
 #include <iostream>
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <string.h>
+
 
 //从socket中读取数据，即recv()数据
 //但是可能出现网络缓冲满了等异常问题，导致数据一次不能完全读取完
@@ -39,7 +44,56 @@ bool TcpRead(const int sockfd, char *buffer, int *ibuflen, const int itimeout = 
 // 返回值：true-成功；false-失败，如果失败，表示socket连接已不可用。
 bool TcpWrite(const int sockfd, const char *buffer, const int *ibuflen = 0);
 
+//socket通信的服务端
+class TcpServer{
+private:
+    int m_socklen;                  //结构体sockaddr_in的大小
+    struct sockaddr_in m_clientaddr;    // 客户端的地址信息
+    struct sockaddr_in m_servaddr;    // 服务店的地址信息
+
+public:
+    int m_listenfd;        //服务端监听的socket
+    int m_connfd;          //客户端连接上来的socket
+    int m_btimeout;        //调用Read和Write方法时，失败的原因是否是超时： true-超时  false-未超时
+    int m_bufflen;          //调用Read后接受到的报文大小，单位：字节
+
+    TcpServer();
+
+    //服务端初始化： port-监听的端口
+    bool initServer(const unsigned int port);
+
+    //阻塞等待客户端的连接请求
+    //返回true-有新的客户端连接   false-失败，accept被中断，如果accept被中断则可以重新accept
+    bool tcpAccept();
+
+    //获取客户端ip地址，返回字符串型"192.168.1.10"这种
+    char *getIp();
+
+    // 接收客户端发送过来的数据。
+    // buffer：接收数据缓冲区的地址，数据的长度存放在m_buflen成员变量中。
+    // itimeout：等待数据的超时时间，单位：秒，缺省值是0-无限等待。
+    // 返回值：true-成功；false-失败，失败有两种情况：1）等待超时，成员变量m_btimeout的值被设置为true；2）socket连接已不可用。
+    bool tcpRead(char *buffer, const int itimeout);
+
+    // 向客户端发送数据。
+    // buffer：待发送数据缓冲区的地址。
+    // ibuflen：待发送数据的大小，单位：字节，缺省值为0，如果发送的是ascii字符串，ibuflen取0，如果是二进制流数据，ibuflen为二进制数据块的大小。
+    // 返回值：true-成功；false-失败，如果失败，表示socket连接已不可用。
+    bool tcpWrite(const char *buffer, const int ibufflen = 0);
+
+    // 关闭监听的socket，即m_listenfd，常用于多进程服务程序的子进程代码中。
+    void closeListen();
+
+    // 关闭客户端的socket，即m_connfd，常用于多进程服务程序的父进程代码中。
+    void closeClient();
+
+    ~TcpServer();  // 析构函数自动关闭socket，释放资源。
+
+};
 
 
+class TcpClient{
+
+};
 
 
